21b6f177942cc321de267c278d192b8a1932b610
Karthik <kvenkata1974@gmail.com>
2016-05-17 09:24:26 +1000

    All

---
+++ oauth2server/src/main/java/com/example/AuthserverApplication.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class AuthserverApplication extends WebMvcConfigurerAdapter {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AuthserverApplication.class, args);
+  }
+
+}
